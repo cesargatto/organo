b68e4438c966e1dfe3e634ee3b029e886ebd4434
César Gatto <105401351+cesargatto@users.noreply.github.com>
2024-02-19 20:31:46 -0300

    Add files via upload

---
+++ src/componentes/Banner/Banner.js
@@ -0,0 +1,18 @@
+
+import { Fragment } from 'react';
+import './Banner.css';
+
+function Banner() {
+    return (
+        <Fragment>
+            <header className="banner">
+                <img src="./imagens/HeaderHeader_total.png" alt='Header da página' className='img_fundo'></img>
+                
+            </header>
+        </Fragment>
+
+    )
+
+}
+
+export default Banner
